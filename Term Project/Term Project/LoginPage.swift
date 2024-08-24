a7025503d9655fb28cb6e19eec40490d72f5f7cb
Win Yu <winyumg2003a@icloud.com>
2024-08-24 23:41:51 +0700

    Added Changes

---
+++ Term Project/Term Project/LoginPage.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Term Project
+//
+//  Created by Win Yu Maung on 24/08/2024.
+//
+
+import UIKit
+
+class LoginPage: UIViewController {
+    @IBOutlet weak var Username: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    @IBOutlet weak var LoginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+        addImage(textField: Username, img: UIImage(systemName: "person.fill")!)
+        addImage(textField: Password, img: UIImage(systemName: "lock.fill")!)
+        LoginButton.layer.cornerRadius = 15
+        LoginButton.configuration?.cornerStyle = .capsule
+        LoginButton.layer.masksToBounds = true
+    }
+    
+    @IBAction func LoginClicked(_ sender: Any) {
+        let Main = storyboard?.instantiateViewController(withIdentifier: "Main") as! MainPage
+        
+        Main.modalPresentationStyle = .popover
+        present(Main, animated: true)
+    }
+    
+    @IBAction func SignupClicked(_ sender: Any) {
+        let signup = storyboard?.instantiateViewController(withIdentifier: "Signup") as! SignupPage
+        
+        
+        signup.modalPresentationStyle = .popover
+        present(signup, animated: true)
+    }
+    
+    @IBAction func ForgetPasswordClicked(_ sender: Any) {
+        let Forget = storyboard?.instantiateViewController(withIdentifier: "ForgetPassword") as! ForgetPasswordPage
+        
+        Forget.modalPresentationStyle = .popover
+        present(Forget, animated: true)
+    }
+    
+}
+
