c12d62bb9560b346f5f516f132997f259b5c085b
Kusk24 <winyumg2003a@gmail.com>
2024-09-01 15:05:38 +0700

    pushed

---
+++ Term Project/Term Project/Main/FavoritePage.swift
@@ -0,0 +1,29 @@
+//
+//  FavoritePage.swift
+//  Term Project
+//
+//  Created by Win Yu Maung on 01/09/2024.
+//
+
+import UIKit
+
+class FavoritePage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
