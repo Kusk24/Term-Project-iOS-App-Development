49c8414e09effd9518ea001fdc7d97e05624e713
Kusk24 <winyumg2003a@gmail.com>
2024-09-07 01:49:22 +0700

    Added account page tableview

--- Term Project/Term Project/Main/AccountPage.swift
+++ Term Project/Term Project/Main/AccountPage.swift
@@ -7,11 +7,38 @@
 
 import UIKit
 
-class AccountPage: UIViewController {
+class AccountPage: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AccountTableCell") as! AccountTableCell
+        let i = indexPath.row
+        if i == 0{
+            cell.textLabel?.text = "Change Username"
+        }else if i == 2{
+            cell.textLabel?.text = "Change Password"
+        } else{
+            cell.textLabel?.text = "Change Email"
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
 
+    @IBOutlet weak var AccountTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        AccountTable.dataSource = self
+        AccountTable.delegate = self
         // Do any additional setup after loading the view.
     }
     
